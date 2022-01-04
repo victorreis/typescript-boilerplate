9f64610dc0a1386be15ccfa9b610f9aa78bb8c37
victor.reis <v.crisostomo.reis@gmail.com>
2022-01-04 13:36:17 -0300

    chore: lint and format @challenge-solving/expressjs-challenges files

--- packages/expressjs-challenges/app.js
+++ packages/expressjs-challenges/app.js
@@ -10,7 +10,11 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
